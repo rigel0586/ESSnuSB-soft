40783afc65086eb953ab08a8b35d44e0b404176e
rigel0586 <rigel0586@gmail.com>
2021-06-15 22:01:47 +0300

    Fix nuance format

--- EsbMacro/EsbSuperFGD/parseExitParticles.C
+++ EsbMacro/EsbSuperFGD/parseExitParticles.C
@@ -1,5 +1,5 @@
-void parseExitParticles(TString inFile = "/*** TODO PATH ***/statsRoot.root"            // Path to statsRoot.root generated from simulate_3_MC_lepton_stats.C
-                        , TString outFile = "/*** TODO PATH ***/formattedData.txt"      // output file where to write the nuance formatted data
+void parseExitParticles(TString inFile = "/home/rigel/opt/ESSnuSB/ESSnuSB-soft/EsbMacro/tests/statsRoot.root"            // Path to statsRoot.root generated from simulate_3_MC_lepton_stats.C
+                        , TString outFile = "/home/rigel/opt/ESSnuSB/ESSnuSB-soft/EsbMacro/tests/formattedData.txt"      // output file where to write the nuance formatted data
                         , Bool_t printData = false)                                     // true - to print the info to the console
 {
     TFile* tf = new TFile(inFile);
@@ -12,19 +12,14 @@ void parseExitParticles(TString inFile = "/*** TODO PATH ***/statsRoot.root"    
 
     if(outputFile.is_open())
 	{
-        
-
         int entries = tr->GetEntries();
         for(int i=0; i < entries; ++i) 
         { 
-            outputFile << "$ begin"  << endl;
-            outputFile << "$ nuance 0"  << endl;
-
             tr->GetEntry(i); 
             
             if(printData)
             {
-                cout << "               Event " << i << "           "<< endl;
+                cout << "                  Event " << i << "           "<< endl;
                 cout << "           Neutrino pdg " << dat->fnuPdg << endl;
                 cout << "        Neutrino Energy " << dat->fnuE << endl;
                 cout << "               IsWeakCC " << dat->fIsWeakCC<< endl;
@@ -34,6 +29,10 @@ void parseExitParticles(TString inFile = "/*** TODO PATH ***/statsRoot.root"    
             
 
             std::vector<esbroot::reconstruction::superfgd::FgdExitParticle>& pars = dat->fVecPars;
+            if(pars.empty()) continue;
+
+            outputFile << "$ begin"  << endl;
+            // outputFile << "$ nuance 0"  << endl;
             for(int j = 0; j < pars.size(); ++j)
             {
                 esbroot::reconstruction::superfgd::FgdExitParticle& p = pars[j];
@@ -44,10 +43,25 @@ void parseExitParticles(TString inFile = "/*** TODO PATH ***/statsRoot.root"    
                     cout << "                   Position " << p.fposition.X() << " " << p.fposition.Y() << " " << p.fposition.Z() << endl;
                 }
                 // Print Vertex
-                outputFile << "$ info 0 " << (j/10) << " " << (j%10) << endl;
-                outputFile << "$ vertex " << p.fposition.X() << " " << p.fposition.Y() << " " << p.fposition.Z() << " 0 " << endl;
-                outputFile << "$ track " << p.fPdg << " " << p.fMomentum.Mag() << " "
-                           << p.fMomentum.X() << " " << p.fMomentum.Y() << " " << p.fMomentum.Z() << " 0 " << endl;
+                // outputFile << "$ info 0 " << (j/10) << " " << (j%10) << endl;
+                // outputFile << "$ vertex " << p.fposition.X() << " " << p.fposition.Y() << " " << p.fposition.Z() << " 0 " << endl;
+                float eMag = p.fMomentum.Mag();
+                float pX = p.fMomentum.X();
+                float pY = p.fMomentum.Y();
+                float pZ = p.fMomentum.Z();
+
+                outputFile << "\t$ track " << p.fPdg << " " << p.fMomentum.Mag() << " "
+                           << (pX/eMag) << " " << (pY/eMag) << " " << (pZ/eMag) << " 0 " << endl;
+
+                // Track format
+                // 1 - pdg of particle
+                // 2 - total energy of the particle
+                // 3 - x direction cosine
+                // 4 - y direction cosine
+                // 5 - z direction cosine
+                // 6 -  0 indicates final state particle after interaction
+                //     -1 initial state particle
+                //     -2 final state particle before interaction
             }
 
             outputFile << "$ end"  << endl;
